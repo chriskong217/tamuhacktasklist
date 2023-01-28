9eeeb4ae858787a50317d0ebfa54357b272d916e
Mukund <mukund@rajesh.ws>
2023-01-28 16:44:38 -0600

    Added login form

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,6 +2,7 @@ import Navbar from './components/Navbar/Navbar.js';
 import TaskList from './components/Tasklist/Task';
 import Home from './components/Home/Home';
 import SignUp from './components/Signup/signup.js';
+import Login from './components/Authentication/Auth';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
@@ -11,6 +12,7 @@ function App() {
         <Navbar />
         <Routes>
 	  <Route exact path="/" exact element={<Home/>} />
+	  <Route path="/login" element={<Login/>} />
 	  <Route path="/signup" element={<SignUp/>} />
           <Route path="/tasklist" element={<TaskList/>} />
         </Routes>
