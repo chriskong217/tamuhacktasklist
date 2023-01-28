5075302495d16d335f2a6260418fc76a6517227e
Mukund <mukund@rajesh.ws>
2023-01-28 14:34:01 -0600

    Fixed Homecd ..cd ..

---
+++ myapp/src/components/Navbar/Navbar.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Nav, NavLink, NavMenu } 
+    from "./NavbarElements";
+  
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavMenu>
+          <NavLink to="/" activeStyle>
+            Home
+          </NavLink>
+          <NavLink to="/tasklist" activeStyle>
+            Task List
+          </NavLink>
+          <NavLink to="/login" activeStyle>
+            Login
+          </NavLink>
+          <NavLink to="/signup" activeStyle>
+            Sign Up
+          </NavLink>
+        </NavMenu>
+      </Nav>
+    </>
+  );
+};
+  
+export default Navbar;
+
