ef8989e5e8e8794c2830844bc1d7da340db5c21b
chriskong217 <xism360@gmail.com>
2023-01-28 15:45:58 -0600

    Update Task.js

--- myapp/src/components/Tasklist/Task.js
+++ myapp/src/components/Tasklist/Task.js
@@ -14,12 +14,19 @@ function TaskList() {
     setNewTask(event.target.value);
   };
 
+  const deleteTask = (index) => {
+    setTasks(tasks.filter((task, i) => i !== index));
+  }
+
   return (
     <div>
       <h1>Task List</h1>
       <ul>
         {tasks.map((task, index) => (
-          <li key={index}>{task}</li>
+          <li key={index}>
+            {task}
+            <button onClick={() => deleteTask(index)}>Delete</button>
+          </li>
         ))}
       </ul>
       <form onSubmit={handleSubmit}>
